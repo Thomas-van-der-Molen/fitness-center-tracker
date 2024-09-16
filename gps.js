c1e87d10899c77b296c87014aa5612697d42afb8
Thomas <58376739+Thomas-van-der-Molen@users.noreply.github.com>
2024-09-16 17:12:02 -0400

    minor changes

--- gps.js
+++ gps.js
@@ -24,7 +24,7 @@ async function giveDataToServer(coords){
   const response = await fetch(url, {
     method: "POST",
     headers: {
-      "Content-Type":"application/x-www-form-urlencoded",
+      "Content-Type":"application/json",
     },
     body: JSON.stringify({lat:coords.latitude.toString(),lon:coords.longitude.toString()})
   });
