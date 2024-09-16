288ab67dc3b95788ac29fcaaaf8580ac06ae7587
Thomas <58376739+Thomas-van-der-Molen@users.noreply.github.com>
2024-09-16 16:18:51 -0400

    add CORS header

--- functions/packages/site_functions/get_data/main.go
+++ functions/packages/site_functions/get_data/main.go
@@ -11,51 +11,57 @@ import (
 	"strconv"
 )
 
-//may need to be negative
-const max_lat float64 = 81.0499630643;
-const min_lat float64 = 81.049240209;
+// may need to be negative
+const max_lat float64 = 81.0499630643
+const min_lat float64 = 81.049240209
 
-const max_lon float64 = 29.1899521529;
-const min_lon float64 = 29.1893445087;
+const max_lon float64 = 29.1899521529
+const min_lon float64 = 29.1893445087
 
-
+type ResponseHeaders struct {
+	CORS string `json:"Access-Control-Allow-Origin"`
+}
 
 type data struct {
 	//boolean to give the user to indicate whether they are at the fitness center
-	User_at_fitness_center bool `json:"User_at_fitness_center"`
-	User_latitude interface{} `json:"User_latitude"`
-	User_longitude interface{} `json:"User_longitude"`
+	User_at_fitness_center bool        `json:"User_at_fitness_center"`
+	User_latitude          interface{} `json:"User_latitude"`
+	User_longitude         interface{} `json:"User_longitude"`
 }
 
 type Response struct {
-	Body data `json:"body"`
+	Body    data            `json:"body"`
+	Headers ResponseHeaders `json:"headers"`
 }
 
 func Main(ctx context.Context, event map[string]interface{}) Response {
-	
+
 	//var user_latitude float32 = event["lat"];
-	
+
 	//user_longitude := event["lon"];
-	
+
 	//wowie, data encoding and datatypes can be hard to manage
- 	latString := event["lat"].(string);
-	lonString := event["lon"].(string);
-	var latFloat, _ = strconv.ParseFloat(latString, 64);
-	var lonFloat, _ = strconv.ParseFloat(lonString, 64);
-	fmt.Println("the type is ", reflect.TypeOf(latFloat));
-	fmt.Println("the value is ", latFloat);
-	
-	var user_at_fitness_center bool;
-	
-	if(latFloat > min_lat && lonFloat > min_lon && latFloat < max_lat && lonFloat < max_lon){
-		user_at_fitness_center = true;
+	latString := event["lat"].(string)
+	lonString := event["lon"].(string)
+	var latFloat, _ = strconv.ParseFloat(latString, 64)
+	var lonFloat, _ = strconv.ParseFloat(lonString, 64)
+	fmt.Println("the type is ", reflect.TypeOf(latFloat))
+	fmt.Println("the value is ", latFloat)
+
+	var user_at_fitness_center bool
+
+	if latFloat > min_lat && lonFloat > min_lon && latFloat < max_lat && lonFloat < max_lon {
+		user_at_fitness_center = true
 	}
 
 	return Response{
 		Body: data{
 			User_at_fitness_center: user_at_fitness_center,
-			User_latitude: event["lat"],
-			User_longitude: event["lon"],
+			User_latitude:          event["lat"],
+			User_longitude:         event["lon"],
+		},
+		Headers: ResponseHeaders{
+			CORS: "*",
 		},
 	}
 }
