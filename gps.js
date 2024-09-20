50807145736bf67cec7d0a5412c050580db67515
Thomas <58376739+Thomas-van-der-Molen@users.noreply.github.com>
2024-09-20 12:04:22 -0400

    debug position check

--- gps.js
+++ gps.js
@@ -13,25 +13,7 @@ function updateDisplay(User_at_fitness_center){
   }
 }
 
-async function giveDataToServer(coords){
-  //console.log("client lat/long datatype");
-  //console.log(typeof coords.latitude);
-  const url = "https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-d8c87cf3-3b1e-403c-86e4-290e57b53ce7/site_functions/get_data";
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type":"application/json",
-    },
-    body: JSON.stringify({lat:coords.latitude.toString(),lon:coords.longitude.toString()})
-  });
-
-  const data = await response.json();
-  console.log("data returned by the server");
-  //console.log(data.User_at_fitness_center);
-  updateDisplay(data.User_at_fitness_center);
-}
-
-lastUserAtGym=false;
+let lastUserAtGym=false;
 function checkUserPos(coords){
   max_lon = -81.0490581547
   min_lon = -81.0501176272
