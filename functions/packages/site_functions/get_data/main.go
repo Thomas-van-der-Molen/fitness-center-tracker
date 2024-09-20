2e4b4fcfa9596e15c157da12ad96cd20393722fd
Thomas <58376739+Thomas-van-der-Molen@users.noreply.github.com>
2024-09-20 10:01:21 -0400

    update serverless function

--- functions/packages/site_functions/get_data/main.go
+++ functions/packages/site_functions/get_data/main.go
@@ -6,10 +6,13 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"reflect"
-	"strconv"
+	_ "fmt"
+	_ "reflect"
+	_ "strconv"
 )
+
+//important global variable that keeps track of the current users at the gym
+var count = 0;
 
 type ResponseHeaders struct {
 	CORS string `json:"Access-Control-Allow-Origin"`
@@ -17,9 +20,10 @@ type ResponseHeaders struct {
 
 type data struct {
 	//boolean to give the user to indicate whether they are at the fitness center
-	User_at_fitness_center bool        `json:"User_at_fitness_center"`
-	User_latitude          interface{} `json:"User_latitude"`
-	User_longitude         interface{} `json:"User_longitude"`
+	//User_at_fitness_center bool        `json:"User_at_fitness_center"`
+	//User_latitude          interface{} `json:"User_latitude"`
+	//User_longitude         interface{} `json:"User_longitude"`
+	Count int `json:"count"`
 }
 
 type Response struct {
@@ -34,18 +38,48 @@ func Main(ctx context.Context, event map[string]interface{}) Response {
 	//user_longitude := event["lon"];
 
 	//wowie, data encoding and datatypes can be hard to manage
-	latString := event["lat"].(string)
-	var latFloat, _ = strconv.ParseFloat(latString, 64)
-	fmt.Println("the type is ", reflect.TypeOf(latFloat))
-	fmt.Println("the value is ", latFloat)
+	//latString := event["lat"].(string)
+	//var latFloat, _ = strconv.ParseFloat(latString, 64)
+	//fmt.Println("the type is ", reflect.TypeOf(latFloat))
+	//fmt.Println("the value is ", latFloat)
 
-	var user_at_fitness_center bool
+	function := event["function"];
+	var returnedCount int;
+	if function == "get_count"{
+		returnedCount = get_count();
+	}else if function == "inc_count"{
+		returnedCount = inc_count();
+	}else if function == "dec_count"{
+		returnedCount = dec_count();
+	}
 
 	return Response{
 		Body: data{
-			User_at_fitness_center: user_at_fitness_center,
-			User_latitude:          event["lat"],
-			User_longitude:         event["lon"],
+			Count: returnedCount,
+		},
+		Headers: ResponseHeaders{
+			CORS: "https://fitness.thomasvandermolen.com",
 		},
 	}
+}
+
+func get_count() int{
+	return count;
+}
+
+func inc_count() int{
+	count++;
+	return count;
+}
+
+func dec_count() int{
+	if count > 0{
+		count--;
+	}
+	return count;
+}
+
+func rst_count() int{
+	count = 0;
+	return count;
 }
